19784252869e880aef2605c6247a0e252bc5ddb7
vineeth1816 <vineeth1816@gmail.com>
2021-04-19 22:57:16 +0530

    Donation Controller

---
+++ FDP-A029/src/main/java/com/example/demo/DaoImpl/FoodDonationDaoImpl.java
@@ -0,0 +1,56 @@
+package com.example.demo.DaoImpl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.Dao.FoodDonationDao;
+import com.example.demo.Model.FoodDonationRequest;
+
+@Component
+public class FoodDonationDaoImpl implements FoodDonationDao{
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public FoodDonationRequest insertRequest(FoodDonationRequest foodDonationRequest) {
+		int flag;
+		try {
+			flag=jdbcTemplate.update("INSERT INTO Food_Donation_Requests(User_ID,Name,Location,Contact_No,Purpose,No_Of_Packets,Status) values(?,?,?,?,?,?,?)", foodDonationRequest.getUserId(),foodDonationRequest.getName(),foodDonationRequest.getLocation(),foodDonationRequest.getContactNo(),foodDonationRequest.getPurpose(),foodDonationRequest.getNoOfPackets(),"Pending for Approval");
+			if(flag>0) {
+				return foodDonationRequest;
+			}
+		}
+		catch (Exception e) {
+			return null;
+		}
+		return null;
+	}
+
+	@Override
+	public List<FoodDonationRequest> getAllRequests() {
+		List<FoodDonationRequest> foodDonationRequests=jdbcTemplate.query("select * from Food_Donation_Requests", new RowMapper<FoodDonationRequest>() {
+
+			@Override
+			public FoodDonationRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FoodDonationRequest fdr=new FoodDonationRequest();
+				fdr.setUserId(rs.getString("User_ID"));
+				fdr.setName(rs.getString("Name"));
+				fdr.setLocation(rs.getString("Location"));
+				fdr.setContactNo(rs.getString("Contact_No"));
+				fdr.setPurpose(rs.getString("Purpose"));
+				fdr.setNoOfPackets(rs.getInt("No_Of_Packets"));
+				return fdr;
+			}
+			
+		});
+		return foodDonationRequests;
+	}
+
+}
--- FDP-A029/src/main/java/com/example/demo/Model/FoodDonationRequest.java
+++ FDP-A029/src/main/java/com/example/demo/Model/FoodDonationRequest.java
@@ -2,4 +2,66 @@ package com.example.demo.Model;
 
 public class FoodDonationRequest {
 
+	private String donationId;
+	private String userId;
+	private String name;
+	private String location;
+	private String contactNo;
+	private String purpose;
+	private int noOfPackets;
+	private String status;
+	public String getDonationId() {
+		return donationId;
+	}
+	public void setDonationId(String donationId) {
+		this.donationId = donationId;
+	}
+	@Override
+	public String toString() {
+		return "FoodDonationRequest [donationId=" + donationId + ", userId=" + userId + ", name=" + name + ", location="
+				+ location + ", contactNo=" + contactNo + ", purpose=" + purpose + ", noOfPackets=" + noOfPackets
+				+ ", status=" + status + "]";
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+	public int getNoOfPackets() {
+		return noOfPackets;
+	}
+	public void setNoOfPackets(int noOfPackets) {
+		this.noOfPackets = noOfPackets;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
