904840db3f959deb0d53c568174c00c5d38b3fbc
vineeth1816 <vineeth1816@gmail.com>
2021-04-19 18:12:32 +0530

    Welcome message

--- FDP-A029/src/main/java/com/example/demo/Controllor/LoginController.java
+++ FDP-A029/src/main/java/com/example/demo/Controllor/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +15,26 @@ import com.example.demo.Service.LoginService;
 
 @RestController
 public class LoginController {
-		
+
 	@Autowired
 	LoginService loginService;
 
-	
-		@PostMapping("/login")
-		@CrossOrigin(origins="http://localhost:4200")
-		public User  registerUser(@RequestBody User user) {
-		
-			User u=loginService.loginUser(user);
-			return u;
-			
-			
-		
+	@PostMapping("/login")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public User registerUser(@RequestBody User user) {
+
+		User u = loginService.loginUser(user);
+		return u;
+
+	}
+
+	@GetMapping("/getUser/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
+	public User registerUser(@PathVariable String id) {
+
+		System.out.println(id);
+		User u = loginService.getUser(id);
+		return u;
+
 	}
 }
