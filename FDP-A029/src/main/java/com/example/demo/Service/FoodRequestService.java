9954c6c786238b7254f4b95f482bd3d9ea7a065f
venumadhavkoigura <venumadhavkoigura@gmail.com>
2021-04-20 21:07:26 +0530

    Food Request Api Completion

---
+++ FDP-A029/src/main/java/com/example/demo/Service/FoodRequestService.java
@@ -0,0 +1,31 @@
+package com.example.demo.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.DaoImpl.FoodDonationDaoImpl;
+import com.example.demo.DaoImpl.FoodRequestDaoImpl;
+import com.example.demo.Model.FoodDonationRequest;
+import com.example.demo.Model.FoodRequest;
+
+@Service
+public class FoodRequestService {
+
+	@Autowired
+	FoodRequestDaoImpl foodRequestDaoImpl;
+	
+	public FoodRequest insertFoodRequest(FoodRequest foodRequest) {
+		System.out.println(foodRequest);
+		return foodRequestDaoImpl.insertFoodRequest(foodRequest);
+	}
+	
+	public List<FoodRequest> getAllFoodRequests(){
+		return foodRequestDaoImpl.getAllFoodRequests();
+	}
+	
+	public List<FoodRequest> getFoodRequestsByUserid(String userid){
+		return foodRequestDaoImpl.getFoodRequestsById(userid);
+	}
+}
