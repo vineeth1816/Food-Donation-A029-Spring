78abf900e76ec9caa30931d776e47ccd7d190a38
vineeth1816 <vineeth1816@gmail.com>
2021-04-24 23:06:30 +0530

    admin requests

--- FDP-A029/src/main/java/com/example/demo/DaoImpl/AdminRequestsDaoImpl.java
+++ FDP-A029/src/main/java/com/example/demo/DaoImpl/AdminRequestsDaoImpl.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
@@ -39,5 +41,125 @@ public class AdminRequestsDaoImpl implements AdminRequestsDao {
 		
 		return foodRequests;
 	}
+
+	@Override
+	public String getRequestId(String requestId, String category) {
+		String rId;
+		rId=jdbcTemplate.query("select * from Admin_Requests where RequestId=? and Category=?", new ResultSetExtractor<String>() {
+
+			@Override
+			public String extractData(ResultSet rs) throws SQLException, DataAccessException {
+				if(rs.next())
+				return rs.getString("RequestId");
+				return null;
+			}
+			
+		}, requestId,category);
+		
+		return rId;
+	}
+
+	@Override
+	public Boolean deleteRequest(String requestId, String category) {
+		int flag;
+		try {
+		flag=jdbcTemplate.update("DELETE FROM Admin_Requests WHERE RequestId=? and Category=?", requestId,category);
+		if(flag>0)
+			return true;
+		return false;
+		}
+		catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public List<FoodRequest> getAllFoodRequestsOrderByLocation(String category) {
+		List<FoodRequest> foodRequests=new ArrayList<FoodRequest>();
+		foodRequests=jdbcTemplate.query("SELECT * FROM Food_Requests  WHERE Request_ID IN(SELECT RequestId FROM Admin_Requests  WHERE Category=?) order by Location",new RowMapper<FoodRequest>() {
+
+			@Override
+			public FoodRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FoodRequest f=new FoodRequest();
+				f.setLocation(rs.getString("Location"));
+				f.setRequestId(rs.getString("Request_ID"));
+				f.setNoOfPackets(rs.getInt("No_Of_Packets"));
+				f.setDate(rs.getDate("Date"));
+				f.setOccation(rs.getString("Occation"));
+				return f;
+			}
+			
+		},category);
+		
+		return foodRequests;
+		
+		
+	}
+
+	@Override
+	public List<FoodRequest> getAllFoodRequestsOrderByQuantity(String category) {
+		List<FoodRequest> foodRequests=new ArrayList<FoodRequest>();
+		foodRequests=jdbcTemplate.query("SELECT * FROM Food_Requests  WHERE Request_ID IN(SELECT RequestId FROM Admin_Requests  WHERE Category=?) order by No_Of_Packets",new RowMapper<FoodRequest>() {
+
+			@Override
+			public FoodRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FoodRequest f=new FoodRequest();
+				f.setLocation(rs.getString("Location"));
+				f.setRequestId(rs.getString("Request_ID"));
+				f.setNoOfPackets(rs.getInt("No_Of_Packets"));
+				f.setDate(rs.getDate("Date"));
+				f.setOccation(rs.getString("Occation"));
+				return f;
+			}
+			
+		},category);
+		
+		return foodRequests;
+		
+	}
+
+	@Override
+	public List<FoodRequest> getAllFoodRequestsOrderByOccation(String category) {
+		List<FoodRequest> foodRequests=new ArrayList<FoodRequest>();
+		foodRequests=jdbcTemplate.query("SELECT * FROM Food_Requests  WHERE Request_ID IN(SELECT RequestId FROM Admin_Requests  WHERE Category=?) order by Occation",new RowMapper<FoodRequest>() {
+
+			@Override
+			public FoodRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FoodRequest f=new FoodRequest();
+				f.setLocation(rs.getString("Location"));
+				f.setRequestId(rs.getString("Request_ID"));
+				f.setNoOfPackets(rs.getInt("No_Of_Packets"));
+				f.setDate(rs.getDate("Date"));
+				f.setOccation(rs.getString("Occation"));
+				return f;
+			}
+			
+		},category);
+		
+		return foodRequests;
+	}
+
+	@Override
+	public List<FoodRequest> getAllFoodRequestsOrderByDate(String category) {
+		List<FoodRequest> foodRequests=new ArrayList<FoodRequest>();
+		foodRequests=jdbcTemplate.query("SELECT * FROM Food_Requests  WHERE Request_ID IN(SELECT RequestId FROM Admin_Requests  WHERE Category=?) order by Date",new RowMapper<FoodRequest>() {
+
+			@Override
+			public FoodRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FoodRequest f=new FoodRequest();
+				f.setLocation(rs.getString("Location"));
+				f.setRequestId(rs.getString("Request_ID"));
+				f.setNoOfPackets(rs.getInt("No_Of_Packets"));
+				f.setDate(rs.getDate("Date"));
+				f.setOccation(rs.getString("Occation"));
+				return f;
+			}
+			
+		},category);
+		
+		return foodRequests;
+	}
+	
+	
 
 }
